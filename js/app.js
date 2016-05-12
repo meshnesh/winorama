530da491ace56273aeee3386f7d2087bba479143
meshnesh <tonnie.nesh@gmail.com>
2016-05-12 14:26:28 +0300

    login functionality

--- js/app.js
+++ js/app.js
@@ -2,3 +2,13 @@ $(document).foundation();
 $(".menu-collapsed").click(function() {
     $(this).toggleClass("menu-expanded");
 });
+
+
+$("#login-button").click(function(event) {
+    $('h1.name').html("<b>Welcome Home</b><br>" + $('#user').val());
+
+    event.preventDefault();
+    $('form').fadeOut(500);
+
+    $('.wrapper').addClass('form-success');
+});
